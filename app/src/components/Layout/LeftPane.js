109d0b9d4c91a0227f8e31c64f4f0d06bcf9d37d
Doran R. Martinez <doranmartinez@gmail.com>
2023-11-09 10:12:32 -0800

    add socials

--- app/src/components/Layout/LeftPane.js
+++ app/src/components/Layout/LeftPane.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import profilePic from "../../../public/images/profile_pic.png"
 
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 class LeftPane extends Component {
     render() {
+        const basic_info = this.props.sharedData.basic_info;
+
+        const socialIcons = {
+            github: GitHubIcon,
+            linkedin: LinkedInIcon,
+        }
+
         return(
             <aside className="left-pane">
                 <div>
                     <a href="/" className="basic-info-name">
-                        <h1>{this.props.sharedData.basic_info.name}</h1>
+                        <h1>{basic_info.name}</h1>
                     </a>
                 </div>
-                <div className="image-container">
+                <div className="profile-pic-container">
                     <img
                         alt="Profile Picture"
                         src={profilePic}
@@ -19,15 +29,22 @@ class LeftPane extends Component {
                 </div>
                 <div className="basic-info-bio">
                     <div>
-                        <p>{this.props.sharedData.basic_info.bio}</p>
+                        <p>{basic_info.bio}</p>
                     </div>
                     <div>
-                        <p>{this.props.sharedData.basic_info.working_on.text}: <br />
-                        <a href="{this.props.sharedData.basic_info.working_on.link}" className="basic-info-project">
-                            {this.props.sharedData.basic_info.working_on.project}
+                        <p>{basic_info.working_on.text}: <br />
+                        <a href={basic_info.working_on.link} className="basic-info-project">
+                            {basic_info.working_on.project}
                         </a>
                         </p>
                     </div>
+                </div>
+                <div className="basic-info-socials">
+                    {basic_info.socials.map((network) => (
+                        <a key={network.name} href={network.url} target="_blank" rel="noopener noreferrer" className={network.class}>
+                            {React.createElement(socialIcons[network.name])}
+                        </a>
+                    ))}
                 </div>
             </aside>
         );
